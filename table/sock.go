a7a12d01d5b329032875f64d055e54640f4011a4
Joe Thomas <josepht404@gmail.com>
2020-05-18 12:31:48 -0700

    ADd in remainder of socket commands

--- table/sock.go
+++ table/sock.go
@@ -1,3 +1,14 @@
+package table
+
+import (
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	HB_INTERVAL = 5 * time.Second
+)
 
 type Sock struct {
 	outQ chan []byte
@@ -7,20 +18,43 @@ type Sock struct {
 	conns map[int64]*websocket.Conn
 }
 
-func (s *Sock) read(conn *websocket.Conn) {
-
+func (s *Sock) read(conn *websocket.Conn, idx int64) {
 	for {
 		_, msg, err := conn.ReadMessage()
 		if err != nil {
 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
 				log.Printf("error: %v", err)
-			}	
+			}
+
+			// TODO: Kill routine for this socket
+			conn.Close()
+			delete(s.conns, idx)
+			break
 		}
+
+		outQ <- msg
 	}
 }
 
-func (s *Sock) write() {
+func (s *Sock) write(conn *websocket.Conn) {
+	hb := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
 
+	for  {
+		select {
+			case <-hb.C:
+				conn.SetWriteDeadline(time.Now().Add(HB_INTERVAL))
+				err := conn.WriteMessage(websocket.PingMessage, nil)
+
+				if err != nil {
+					return
+				}
+			}
+		}
+	}
 }
 
 func (s *Sock) AddConnection(w http.ResponseWriter, r *http.Request) {
@@ -30,8 +64,10 @@ func (s *Sock) AddConnection(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	s.conns[s.connCounter++] = conn
-	go s.read(conn)
+	idx := s.connCounter++
+	s.conns[idx] = conn
+
+	go s.read(conn, idx)
 }
 
 func (s *Sock) Read() []byte {
